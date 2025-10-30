be780d64387a9e9110a5160cf0802041e56398a6
soporte-glitch <soporte@hidalgograduaciones.com>
2025-10-29 21:39:41 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-
+ base: '/HidalgoPhotoIA4.0/',
 export default defineConfig(({ mode }) => {
+
     const env = loadEnv(mode, '.', '');
     return {
       server: {
